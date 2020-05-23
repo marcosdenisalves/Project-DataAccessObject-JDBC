8d55786301cdb67c94ef5d702080cc93ae4ff9a5
Marcos Dênis <dee.alves2@gmail.com>
2020-05-22 23:30:53 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+
 		Department obj = new Department(1, "Books");
+		Seller sl = new Seller(1, "Maria", "maria@gmail.com", new Date(), 2500.0, obj);
+		
 		System.out.println(obj);
+		System.out.println();
+		System.out.println(sl);
 	}
 
 }
